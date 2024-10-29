a8de511122c7176ac4f4daa37dc62f7cd04329c6
Bastiaan Quast <bquast@valiq.com>
2024-10-29 16:02:58 +0100

    add form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script>
+  let email = '';
+  let facebook = '';
+  let instagram = '';
+  let twitter = '';
+</script>
+
+<form method="POST" action="/api/submit">
+  <input type="email" bind:value={email} name="email" placeholder="Enter your email" required />
+  <input type="text" bind:value={facebook} name="facebook" placeholder="Facebook URL" />
+  <input type="text" bind:value={instagram} name="instagram" placeholder="Instagram URL" />
+  <input type="text" bind:value={twitter} name="twitter" placeholder="Twitter URL" />
+  <button type="submit">Submit</button>
+</form>
